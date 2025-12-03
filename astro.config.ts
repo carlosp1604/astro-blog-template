2da6aa78107b05cacb9876423baba45ff6faab92
Carlos Ponton <carlitosponton@hotmail.com>
2025-12-03 10:39:31 +0100

    Use env loader instead of vite env vars to solve sitemap generation issue

--- astro.config.ts
+++ astro.config.ts
@@ -3,12 +3,15 @@ import mdx from '@astrojs/mdx'
 import vercel from '@astrojs/vercel'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
+import { env } from './env.loader.ts'
 import { defineConfig } from 'astro/config'
 import { i18nGlobalConfig } from './i18n.global.config'
 
+const { PUBLIC_SITE_BASE_URL } = env
+
 // https://astro.build/config
 export default defineConfig({
-  site: import.meta.env.PUBLIC_SITE_BASE_URL,
+  site: PUBLIC_SITE_BASE_URL,
   output: 'server',
   vite: {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -33,8 +36,8 @@ export default defineConfig({
         }, {})
       },
       filter: (page) =>
-        page !== import.meta.env.PUBLIC_SITE_BASE_URL + '/404/' &&
-        page !== import.meta.env.PUBLIC_SITE_BASE_URL + '/500/'
+        page !== PUBLIC_SITE_BASE_URL + '/404/' &&
+        page !== PUBLIC_SITE_BASE_URL + '/500/'
     })
   ],
   trailingSlash: 'always',
